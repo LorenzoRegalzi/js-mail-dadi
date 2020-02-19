c1d629a3148adc293d27d6f8f517f5fab6a66ffc
l <lorenzo.regalzi@gmail.com>
2020-02-19 22:56:31 +0100

    secondo esercizio stilizzato

--- ex2/js/main.js
+++ ex2/js/main.js
@@ -1,16 +1,19 @@
 alert('premi per giocare')
 var numeroUtente = Math.random()*10;
 console.log('il tuo numero e '+numeroUtente.toFixed(0));
+document.getElementById('Primo').innerHTML = numeroUtente.toFixed(0)+' numero utente';
 var pc = Math.random()*10;
 console.log('il numero del pc e '+ pc.toFixed(0));
-
+document.getElementById('Secondo').innerHTML = pc.toFixed(0) + ' numero del pc';
 
 
 
 if (numeroUtente>pc) {
     console.log('hai vinto tu!');
-} else if  (numeroUtente == pc) {
+    document.getElementById('vittoria').innerHTML ='hai vinto tu';
+} else if  (numeroUtente === pc) {
     console.log('pareggio');
 } else {
     console.log('ha vinto il pc');
+    document.getElementById('sconfitta').innerHTML ='hai perso';
 }
