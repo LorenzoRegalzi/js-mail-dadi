d796e0608fc2193544d03c896792fb631c205560
l <lorenzo.regalzi@gmail.com>
2020-02-19 23:06:32 +0100

    primo esercizio stilizzato

--- ex1/js/main.js
+++ ex1/js/main.js
@@ -5,10 +5,13 @@ console.log(pass);
 
 if (risposta == pass [0]) {
     console.log('accesso corretto');
+    document.getElementById('corretta').innerHTML= "e-mail corretta";
 }
 else if (risposta == pass [1]) {
     console.log('accesso corretto');
+    document.getElementById('corretta').innerHTML = "e-mail corretta";
 }
 else {
     console.log('email sbagliata');
+    document.getElementById('errata').innerHTML = "e-mail sbagliata";
 }
